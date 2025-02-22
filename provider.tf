01342f7d7dd6815267cda041fb62c076b09dd514
Tonny Luiz <tonnyluiz00@gmail.com>
2025-02-22 11:17:55 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,16 +1,4 @@
-provider "google" {
-  #credentials = file("key.json")
-  project = "curso-terraform-tonny"
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}
-
 terraform {
-  backend "gcs" {
-    bucket = "bucket-tonny"
-    prefix = "projetospringdockops-with-firewall-rules"
-  }
-
   required_providers {
     google = {
       source  = "hashicorp/google"
